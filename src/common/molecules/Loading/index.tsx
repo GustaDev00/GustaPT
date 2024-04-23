900f525c5f5b7a0d6e8e9a40af52b1c9012b337a
Gustavo Pontes <gustavopontes@mbp-de-gustavo.lan>
2024-04-23 16:29:48 -0300

    feat(load): useEffectResize

--- src/common/molecules/Loading/index.tsx
+++ src/common/molecules/Loading/index.tsx
@@ -12,6 +12,22 @@ const Loading = () => {
   if (!text) return <></>;
 
   const Squares = () => {
+    useEffectResize(
+      () => {
+        const squareSize = window.innerWidth > 600 ? 100 : 50;
+
+        const width = window.innerWidth + 100;
+        const height = window.innerHeight;
+        const numSquaresWidth = Math.ceil(width / squareSize);
+        const numSquaresHeight = Math.ceil(height / squareSize);
+        const totalSquares = numSquaresWidth * numSquaresHeight;
+
+        setNumSquares(totalSquares);
+      },
+      [],
+      true,
+    );
+
     return (
       <S.SquaresContainer>
         {Array.from({ length: numSquares }, (_, i) => (
@@ -25,22 +41,6 @@ const Loading = () => {
       </S.SquaresContainer>
     );
   };
-
-  useEffectResize(
-    () => {
-      const squareSize = window.innerWidth > 600 ? 100 : 50;
-
-      const width = window.innerWidth + 100;
-      const height = window.innerHeight;
-      const numSquaresWidth = Math.ceil(width / squareSize);
-      const numSquaresHeight = Math.ceil(height / squareSize);
-      const totalSquares = numSquaresWidth * numSquaresHeight;
-
-      setNumSquares(totalSquares);
-    },
-    [],
-    true,
-  );
 
   return (
     <S.Loading ref={containerRef}>
